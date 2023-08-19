49cadae92dfad20f2616148ee7869fb641a753ef
Princianna <princiannadurosier@gmail.com>
2023-08-19 12:21:17 -0700

    update

---
+++ Buleseye/Views/Shapes.swift
@@ -0,0 +1,34 @@
+//
+//  Shapes.swift
+//  Buleseye
+//
+//  Created by Princianna on 8/15/23.
+//
+
+import SwiftUI
+
+struct Shapes: View {
+    var body: some View {
+        VStack {
+            Circle()
+                .strokeBorder(Color.blue, lineWidth: 20)
+                .frame(width: 200, height: 100)
+                .background(Color.green)
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.blue)
+                .frame(width:200, height:100)
+            Capsule()
+                .fill(Color.blue)
+                .frame(width:200, height:100)
+            Ellipse()
+                .fill(Color.blue)
+                .frame(width:200, height:100)        }
+        
+    }
+}
+
+struct Shapes_Previews: PreviewProvider {
+    static var previews: some View {
+        Shapes()
+    }
+}
