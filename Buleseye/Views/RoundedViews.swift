05c6fde75174e66f79bbca06c6a4f59a1db72092
Princianna <princiannadurosier@gmail.com>
2023-08-21 21:20:00 -0700

    Arrow and line text background colors in assets category

--- Buleseye/Views/RoundedViews.swift
+++ Buleseye/Views/RoundedViews.swift
@@ -8,6 +8,10 @@ struct RoundedImageViewStroked : View {
             .font(.title)
             .foregroundColor(Color("TextColor"))
             .frame(width: 56, height: 56)
+            .overlay(
+            Circle()
+                .strokeBorder(Color("ButtonStrokedColor"), lineWidth: 2)
+            )
     }
 }
 struct RoundedImageViewFilled : View {
@@ -16,8 +20,12 @@ struct RoundedImageViewFilled : View {
     var body: some View {
         Image(systemName: systemName)
             .font(.title)
-            .foregroundColor(Color("TextColor"))
+            .foregroundColor(Color("ButtonFilledTextColor"))
             .frame(width: 56, height: 56)
+            .background(
+                Circle()
+                    .fill(Color("ButtonFilledBackgroundColor"))
+            ) 
     }
 }
 
